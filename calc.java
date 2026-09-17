e8898088b16a660069f25b7e5cf114499b2cbaee
RykerTheCoder <rykernaser5@gmail.com>
2026-09-16 18:51:32 -0700

    Implement basic calculator functionality

--- calc.java
+++ calc.java
@@ -1,1 +1,40 @@
+import java.util.Scanner;
 
+public class calc {
+    public static void main(String[] args) {
+
+        boolean as = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while (as == true) { //checks if exit is typed
+
+            System.out.print("Solve: ");
+
+            String firstInput = scanner.next();
+
+            if ("exit".equals(firstInput)) { // stops everything if exit is typed
+                System.out.println("Exiting");
+                as = false;
+                continue;
+            }
+       
+            String operator = scanner.next();
+            double secondInput = scanner.nextDouble();
+
+            double a = 0;
+            double firstNum = Double.parseDouble(firstInput);
+            switch (operator) {
+                case "+" -> a = firstNum + secondInput; // all of the calculations
+                case "-" -> a = firstNum - secondInput;
+                case "*" -> a = firstNum * secondInput;
+                case "/" -> a = firstNum / secondInput;
+                default -> System.out.println("Invalid operator");
+            }
+
+            System.out.println(firstNum + " " + operator + " " + secondInput + " = " + a); 
+
+        }
+        
+       
+    }
+}
